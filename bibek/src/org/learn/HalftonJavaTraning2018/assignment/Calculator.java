3fcc1c786765719406a9dab2f7fc9e9df18d3da7
bibek <sbibek100@gmail.com>
2018-06-06 11:11:07 +0545

    1_bibek_calculator "calculator added"

--- bibek/src/org/learn/HalftonJavaTraning2018/assignment/Calculator.java
+++ bibek/src/org/learn/HalftonJavaTraning2018/assignment/Calculator.java
@@ -1,10 +1,81 @@
 package org.learn.HalftonJavaTraning2018.assignment;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
+		float firstNumber;
+		float secondNumber;
+		float result;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+			System.out.println("Enter the first  number");
+			
+			
+			
+			firstNumber=sc.nextFloat();
+			if(firstNumber<0) {
+				System.out.println("You cannot enter negative number. Please enter positive number");
+				System.out.println("Enter the first  number");
+				firstNumber=sc.nextFloat();
+			}
+			System.out.println("Enter the second  number");
+			secondNumber=sc.nextFloat();
+			if(secondNumber<0) {
+				System.out.println("You cannot enter negative number. Please enter positive number");
+				System.out.println("Enter the second  number");
+				secondNumber=sc.nextFloat();				
+			}
+//			continue;
+//			System.out.println("Enter the second +ve number");
+//			secondNumber=sc.nextFloat();
+//			continue;
+		
+	
+			
+			System.out.println("Enter operator (+, -, *, /): ");
+			char a=sc.next().charAt(0);
+			
+			switch(a)
+		{
+			case '+':
+				result=firstNumber+secondNumber;
+				System.out.println("Addition of two number is "+result);
+				break;
+			case '-':
+				result=firstNumber-secondNumber;
+				System.out.println("Addition of two number is "+result);
+				break;
+			case '*':
+				result=firstNumber*secondNumber;
+				System.out.println("Addition of two number is "+result);
+				break;
+			case '/':
+				result=firstNumber/secondNumber;
+				System.out.println("Addition of two number is "+result);
+				break;
+			default:
+				System.out.println("plz enter valid operator");
+		
 	}
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	
+
+	
 
 }
+
