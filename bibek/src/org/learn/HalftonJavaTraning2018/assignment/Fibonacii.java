64f101230ffb8aacc3b16059002be63bb4402ba8
bibek <sbibek100@gmail.com>
2018-06-06 16:25:13 +0545

    9_bibek fibonacii series commited

---
+++ bibek/src/org/learn/HalftonJavaTraning2018/assignment/Fibonacii.java
@@ -0,0 +1,21 @@
+package org.learn.HalftonJavaTraning2018.assignment;
+
+public class Fibonacii {
+
+	public static void main(String[] args) {
+		
+		
+		int n1=0,n2=1,n3,i,count=100;    
+		 System.out.print(n1+" "+n2);   
+		    
+		 for(i=2;i<count;++i)  
+		 {    
+		  n3=n1+n2;    
+		  System.out.print(" "+n3);    
+		  n1=n2;    
+		  n2=n3;    
+
+	}
+
+}
+}
