0995f0d89907068facc26fc7a0d80d6812a1f83e
bibek <sbibek100@gmail.com>
2018-06-11 18:41:05 +0545

    #19 code is formatted

--- bibek/src/org/learn/HalftonJavaTraning2018/assignment/TrafficLight.java
+++ bibek/src/org/learn/HalftonJavaTraning2018/assignment/TrafficLight.java
@@ -1,29 +1,22 @@
 package org.learn.HalftonJavaTraning2018.assignment;
 
- public enum TrafficLight {
-	
-	RED("Stop",10),
-	GREEN("Go",10),
-	YELLOW("Slow",5);
-	
-	private  final String commands;
-	private  final int time;
-	
-	TrafficLight(String commands,int time)
-	{
-		this.commands=commands;
-		this.time=time;
+public enum TrafficLight {
+
+	RED("Stop", 10), GREEN("Go", 10), YELLOW("Slow", 5);
+
+	private final String commands;
+	private final int time;
+
+	TrafficLight(String commands, int time) {
+		this.commands = commands;
+		this.time = time;
 	}
-	
-	public String getCommands()
-	{
+
+	public String getCommands() {
 		return commands;
 	}
-	
-	public int getTime()
-	{
+
+	public int getTime() {
 		return time;
 	}
 }
- 
-
--- bibek/src/org/learn/HalftonJavaTraning2018/assignment/TrafficLightExecutation.java
+++ bibek/src/org/learn/HalftonJavaTraning2018/assignment/TrafficLightExecutation.java
@@ -3,22 +3,21 @@ package org.learn.HalftonJavaTraning2018.assignment;
 public class TrafficLightExecutation {
 
 	public static void main(String[] args) throws InterruptedException {
-		while(true) {
+		while (true) {
 			TrafficLight signal = TrafficLight.GREEN;
-			System.out.println(signal.getCommands()); 
+			System.out.println(signal.getCommands());
 			System.out.println(signal.getTime());
 			Thread.sleep(10000);
 			signal = TrafficLight.YELLOW;
-			System.out.println(signal.getCommands()); 
+			System.out.println(signal.getCommands());
 			System.out.println(signal.getTime());
 			Thread.sleep(50000);
 			signal = TrafficLight.RED;
-			System.out.println(signal.getCommands()); 
+			System.out.println(signal.getCommands());
 			System.out.println(signal.getTime());
 			Thread.sleep(10000);
 		}
-		
-		
+
 	}
 
 }
