bd515b19063cc9b7d50564191873e50c820946ec
Simon Shrestha <shrestha.simon25@gmail.com>
2018-06-15 15:48:06 +0545

    9_fibonacci series

--- simon/src/com/halftonedesign/trainning/Fibonacci.java
+++ simon/src/com/halftonedesign/trainning/Fibonacci.java
@@ -1,8 +1,20 @@
 package com.halftonedesign.trainning;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		int n1=0,n2=1,n=0,a=0;
+		System.out.println("Please enter a number: ");
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		do {
+			System.out.println(n1);
+			n=n1+n2;
+			n1=n2;
+			n2=n;
+		} while(n1<=a);
+		scan.close();
 	}
 
 }
--- simon/src/com/halftonedesign/trainning/Sum.java
+++ simon/src/com/halftonedesign/trainning/Sum.java
@@ -1,9 +1,21 @@
 package com.halftonedesign.trainning;
 
+import java.util.Scanner;
+
 public class Sum {
 	public static void main(String[] args) {
-		int a;
-		int sum;
-		Scanner scan = new Scanner
+		long sum = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number");
+		long a = scan.nextInt();
+		long startTime = System.currentTimeMillis();
+		for (long i = 1; i < a + 1; i++) {
+			sum = sum + i;
+		}
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+		System.out.println("Total time taken: " + elapsedTime);
+		System.out.println("The sum of numbers from 1 to " + a + " is: " + sum);
+		scan.close();
 	}
 }
