07c67af5cba90148497ac323b9278ab0ceef9215
jyoti <pudasainijoti@gmail.com>
2018-06-19 17:33:27 +0545

    not completely correct

---
+++ jyoti/src/Task.java
@@ -0,0 +1,18 @@
+
+public class Task {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6,7,8,10};
+		for(int i=0;i<=9;i++) {
+			if(a[i]+a[i+1]==7){
+				System.out.print(a[i]+" "+a[i+1]);
+		
+				
+				
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
